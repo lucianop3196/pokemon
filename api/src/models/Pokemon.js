d7a427447bb70b7a2bc177ade7e89709969e5bb7
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-01-30 15:13:24 -0300

    Models: pokemons and types, are created

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,46 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define("pokemon", {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      // unique: true, ???
+      validate: {
+        //Solo letras, empezando con mayscula
+        is: /^[A-Za-z]+$/,
+      },
+    },
+    life: {
+      type: DataTypes.INTEGER,
+    },
+    strength: {
+      type: DataTypes.INTEGER,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+    },
+  });
+
+  sequelize.define("type", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        //Solo letras, empezando con mayscula
+        is: /^[A-Za-z]+$/,
+      },
     },
   });
 };
