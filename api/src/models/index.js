2710c3d23c3024eb230465e977d729c64977ec5c
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-01-31 13:19:34 -0300

    id (UUID) is defined for model Pokemon + route get("/pokemon")

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -4,6 +4,12 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      unique: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,37 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const axios = require("axios");
+//const { Model } = require("sequelize/dist");
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/pokemons", async (req, res) => {
+  try {
+    const data = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    const pokemonsDataApi = data.data.results; //[{name, url}, {name2, url2}]
+    const pokemonsDataApiPromises = pokemonsDataApi?.map((pokemon) => {
+      // Hay un problema con el map, promise <pending>
+      return axios
+        .get(pokemon.url)
+        .then((response) => {
+          const types = response.data.types?.map((elem) => elem.type.name); //[type1, type2]
+          const urlImg = response.data.sprites.other.dream_world.front_default; //"img_URL"
+          return { name: pokemon.name, type: types, img: urlImg };
+        })
+        .catch((e) => console.log(e));
+    });
+    const pokemonsApi = Promise.all(pokemonsDataApiPromises).then(
+      (value) => res.json(value)
+      //Debo retornar [{name,type:[type1, type2] {, img}, {name, type, img}]
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
