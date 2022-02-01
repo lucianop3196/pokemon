5d937c2a34dfaf1be140d92a69fdbd9f4383e42c
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-01-31 21:53:47 -0300

    Promise.all() is implemented for multiple axios.get(/pokemons)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,28 +9,62 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const showDataApi = (response) => {
+  return {
+    types: response.data.types?.map((elem) => elem.type.name),
+    urlImg: response.data.sprites.other["official-artwork"].front_default,
+    id: response.data.id,
+    height: response.data.height,
+    weight: response.data.weight,
+  };
+};
 
 router.get("/pokemons", async (req, res) => {
+  //name, img, tipos
   try {
-    const data = await axios.get("https://pokeapi.co/api/v2/pokemon");
-    const pokemonsDataApi = data.data.results; //[{name, url}, {name2, url2}]
+    const dataApi = await Promise.all([
+      axios.get("https://pokeapi.co/api/v2/pokemon"),
+      axios.get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"),
+    ]);
+    // console.log(datApi) ---> [{ data: {results:[] } }, { data:{results:[] } }]
+
+    dataArr1 = dataApi[0].data.results;
+    dataArr2 = dataApi[1].data.results;
+
+    const pokemonsDataApi = dataArr1.concat(dataArr2); //[{name, url}, {name2, url2}, ...]
     const pokemonsDataApiPromises = pokemonsDataApi?.map((pokemon) => {
       // Hay un problema con el map, promise <pending>
       return axios
         .get(pokemon.url)
         .then((response) => {
-          const types = response.data.types?.map((elem) => elem.type.name); //[type1, type2]
-          const urlImg = response.data.sprites.other.dream_world.front_default; //"img_URL"
+          const { types, urlImg } = showDataApi(response);
           return { name: pokemon.name, type: types, img: urlImg };
         })
         .catch((e) => console.log(e));
     });
-    const pokemonsApi = Promise.all(pokemonsDataApiPromises).then(
-      (value) => res.json(value)
-      //Debo retornar [{name,type:[type1, type2] {, img}, {name, type, img}]
-    );
+    const pokemonsApi = await Promise.all(pokemonsDataApiPromises);
+    res.json(pokemonsApi);
   } catch (error) {
     console.log(error);
+  }
+});
+
+router.get("/pokemon/:idPokemon", async (req, res) => {
+  const { idPokemon } = req.params;
+  try {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${idPokemon}`
+    );
+    if (response) {
+      const { types, urlImg, id, height, weight } = showDataApi(response);
+      const stats = response.data.stats.map((stat) => {
+        return {};
+      });
+
+      res.json("ok");
+    }
+  } catch (e) {
+    console.log(e);
   }
 });
 
