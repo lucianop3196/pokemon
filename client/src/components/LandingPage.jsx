e53c48132f63906698b90d1b1d554b5f74547dc9
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-02-17 19:56:35 -0300

    Indication in home button from LandingPage

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import { background, homeBtn } from "../styles/LandingPage.module.css";
+import { background, homeBtn, home_span } from "../styles/LandingPage.module.css";
 
 function LandingPage() {
   return (
     <div className={background}>
-      
       <Link to="/home">
-      <div className={homeBtn}></div>
+        <div className={homeBtn}></div>
+        <span className={home_span}>Home</span>
       </Link>
     </div>
   );
